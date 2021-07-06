fa8f69e487267259de4f246ed918aefdd2e9d6f2
Yasmin Khalaf <yasmin.h.khalaf@gmail.com>
2021-07-05 22:12:43 -0300

    Challenges 03 - done

---
+++ desafios_03/data.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    id: "mapas-com-react-usando-leaflet",
+    image:
+      "https://blog.rocketseat.com.br/content/images/2020/11/blog-thumb-utilizando-mapas-no-react-com-leaflet-1.jpg",
+    title: "Mapas com React usando Leaflet",
+    author: "Thiago Marinho",
+    duration: "11 mins",
+  },
+  {
+    id: "dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida",
+    image:
+      "https://blog.rocketseat.com.br/content/images/2020/10/dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida.jpg",
+    title:
+      "Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida",
+    author: "Thiago Marinho",
+    duration: "3 mins",
+  },
+  {
+    id: "es11-novas-features-do-javascript",
+    image:
+      "https://blog.rocketseat.com.br/content/images/2020/09/es11-novas-features-do-javascript.png",
+    title: "ES11 - Novas features do JavaScript",
+    author: "Thiago Marinho",
+    duration: "8 mins",
+  },
+  {
+    id: "como-renomear-varios-arquivos-de-uma-vez-usando-o-terminal",
+    image:
+      "https://blog.rocketseat.com.br/content/images/2020/11/thumb-como-renomear-varios-arquivos-de-uma-vez-usando-o-terminal.jpg",
+    title: "Como renomear vários arquivos de uma vez usando o terminal",
+    author: "Thiago Marinho",
+    duration: "8 mins",
+  },
+];
--- desafios_03/server.js
+++ desafios_03/server.js
@@ -2,21 +2,51 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 
 const server = express();
-
+const posts = require("./data");
 server.use(express.static("public"));
 
 server.set("view engine", "njk");
-nunjucks.configure("views", { express: server });
+nunjucks.configure("views", { express: server, noCache: true });
 
 server.get("/", (req, res) => {
-  return res.render("conteudos");
+  return res.render("courses", { posts });
 });
 
 server.get("/about", (req, res) => {
-  return res.render("about");
+  const profile = {
+    image: "https://github.com/rocketseat-education.png",
+    name: "Rocketseat Education",
+    about: `Organização que armazena o código das aplicações e desafios dos
+        bootcamps e de cursos gratuitos da Rocketseat 🚀`,
+    links: [
+      {
+        name: "Github",
+        url: "https://www.github.com/yasmink88",
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/yasminkhalaf",
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/yasminkhalaf88",
+      },
+    ],
+  };
+  return res.render("about", { profile });
 });
 
-server.use(function (req, res) {
+server.get("/course", (req, res) => {
+  const id = req.query.id;
+
+  const post = posts.find((post) => {
+    return post.id == id;
+  });
+
+  return res.render("course", { post });
+});
+
+server.use((req, res) => {
   res.status(404).render("not-found");
 });
 
